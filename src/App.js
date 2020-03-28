b7d304df7861db4590d9baac04cd6022c2470e7e
Joshua Lawrence <joshua98lawrence@gmail.com>
2020-03-27 22:23:54 -0600

    Added Access to the GitHub API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const CardList = (props) => (
+  <div>
+    {props.profiles.map(profile => <Card key={profile.id} {...profile}/>)}
+  </div>
+);
+
+class Card extends React.Component {
+  render() {
+    const profile = this.props;
+    return (
+      <div className="github-profile">
+        <img src={profile.avatar_url}/>
+        <div className="info">
+            <div className="name">{profile.name}</div>
+            <div className="company">{profile.company}</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+class SearchForm extends React.Component {
+  state = { userName: '' };
+  handleSubmit = async (e) => {
+    e.preventDefault();
+    const resp = await axios.get(`https://api.github.com/users/${this.state.userName}`);
+    this.props.onSubmit(resp.data);
+    this.setState({ userName: '' })
+  };
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit} className='SearchForm'>
+        <input 
+          type='text' 
+          value={this.state.userName}
+          onChange={event => this.setState({ userName: event.target.value })}
+          placeholder='GitHub username' 
+          required 
+        />
+        <button>Add Card</button>
+      </form>
+    );
+  }
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      profiles: [],
+    };
+  }
+  addNewProfile = (profileData) => {
+    this.setState(prevState => ({
+        profiles: [...prevState.profiles, profileData],
+    }));
+  };
+  render () {
+    return (
+      <div className="App">
+        <header className="App-header">
+          {this.props.title}
+        </header>
+        <div className="Content">
+          <SearchForm onSubmit={this.addNewProfile}/>
+          <CardList profiles={this.state.profiles}/>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App title="GitHub Person Search"/>
   </React.StrictMode>,
   document.getElementById('root')
 );
